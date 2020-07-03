38cd5dde08152ab2fd86e668b003b36debdec3dd
Vlo <vlo.galstyan.2013@mail.ru>
2020-07-03 16:59:16 +0400

    update

--- routes/personal.js
+++ routes/personal.js
@@ -3,17 +3,22 @@ const router = Router();
 const Contact = require('../models/Contact');
 const Category = require('../models/Category')
 const Logo = require('../models/Logo')
+const auth = require('../middleware/auth');
 
-router.get('/', async  (req, res) => {
+router.get('/', auth, async  (req, res) => {
     const contact = await Contact.find()
     const category = await Category.find();
     const logo = await Logo.find();
+
+    const user = req.user.toObject();
+
 
     res.render('personal', {
         title: 'Личный Кабинет',
         contact,
         category,
-        logo
+        logo,
+        user
     })
 })
 
